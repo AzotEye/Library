8fc3e3be7a1022a359ca21d0573942eaf460ef81
Azot Eye <Azots@hotmail.com>
2024-03-27 15:34:54 +0300

    fixWcomments

--- LibraryC/LibraryC/Book.h
+++ LibraryC/LibraryC/Book.h
@@ -1,14 +1,27 @@
 #pragma once
 #define L 50
 
+typedef enum
+{
+    FANTASY = 0,
+    HORROR = 1,
+    HISTORY = 2,
+} SectionType;
+
+typedef enum
+{
+    TRUE = 1,
+    FALSE = 0,
+} Presence;
+
 typedef struct
 {
-    char author[L];
-    char publisher[L];
-    char name[L];
+    char author[L]; // char*
+    char publisher[L]; // char*
+    char name[L]; // char*
 
-    char section[L];
+    char section[L]; // SectionType section;
 
     double review;
-    char presense[L];
+    char presense[L]; // Presence isAvailable
 }Book;
--- LibraryC/LibraryC/Func.h
+++ LibraryC/LibraryC/Func.h
@@ -2,11 +2,23 @@
 
 #include "Book.h"
 
+typedef struct
+{
+    Book* books;
+    int count;
+} Library;
+
+
 void fillLibrary(const char* filename, Book * Library);
+// void fillLibrary(const char* filename, Library* lib);
 
 void printBook(Book book);
+// void printBook(const Book* book);
 void printSection(Book* Library, const char * section, int* idx);
+// void getSectionBooks(Library* lib, SectionType section, int** idx, int *n);
 
 void selectName(int * idx, Book * Library);
+// void selectByName(int* idx, int n, Library* lib);
 int selectSection();
 void selectBook(Book* Library);
+// void selectBook(Library* lib);
